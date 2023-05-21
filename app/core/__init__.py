482c176b29835e8208c3a9c1463c4c9723172bcb
john62821 <johndavid62821@gmail.com>
2023-05-21 18:57:10 -0400

    ADD: General doc for Fast API

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -3,8 +3,15 @@ import uvicorn
 from core.manage import settings
 from api.v1.controllers.telegram_bot_webhook import webhooks_router
 
+
 # Start APP 
-app = FastAPI(docs_url="/api-docs")
+app = FastAPI(
+        title="Telegram Bot API",
+        description="a REST API using python for Telegram Bot",
+        version="0.0.1",
+        openapi_tags=settings.TAGS_METADATA,
+        docs_url="/api-docs",
+    )
 
 # Routers
 app.include_router(webhooks_router, prefix="/api/v1")
