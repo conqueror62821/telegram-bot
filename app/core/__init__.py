bba7e94f54de3a21af2b06149e7be7d30edf1152
john62821 <johndavid62821@gmail.com>
2023-05-21 18:40:17 -0400

    ADD: Auto reload server

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 import uvicorn
 from core.manage import settings
 from api.v1.controllers.telegram_bot_webhook import webhooks_router
+from api.v1.controllers.items import items_router
 
 # Start APP 
 app = FastAPI(docs_url="/api-docs")
 
 # Routers
 app.include_router(webhooks_router, prefix="/api/v1")
+app.include_router(items_router, prefix="/api/v1")
 
 
-def run_server(): uvicorn.run(app, host=settings.HOST, port=8000)
+def run_server(): uvicorn.run('core:app',host=settings.HOST,port=8000,reload=True)
