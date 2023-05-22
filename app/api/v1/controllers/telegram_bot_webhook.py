ea143c751c85196bf7bf43876811d54c96bc766b
john62821 <johndavid62821@gmail.com>
2023-05-21 21:20:29 -0400

    FIX: setting references

--- app/api/v1/controllers/telegram_bot_webhook.py
+++ app/api/v1/controllers/telegram_bot_webhook.py
@@ -1,18 +1,24 @@
 from fastapi import Request
 import telegram
 from core.routers import webhooks_router
+from models.telegram_custom_bot import TelegramCustomBot
 
+bot = TelegramCustomBot()
 
 @webhooks_router.post(
     "/webhook-messages",
     tags=["webhooks"],
     response_model=None,
-    description="Telegam messages webhook.",
+    description="Telegram messages webhook.",
 )
 async def webhook(request: Request):
+    print("BOTSITO",bot)
+
     update = telegram.Update.de_json(await request.json(), bot)
     message = update.message
-    
+
+    print(message.message_id)
+    # Skip messages
     if message.message_id == bot.last_message_id: return
     
     # Process
@@ -27,4 +33,3 @@ async def webhook(request: Request):
     await bot.send_message()
 
     return {'status' : 'OK'}
-
--- app/models/telegram_custom_bot.py
+++ app/models/telegram_custom_bot.py
@@ -1,5 +1,5 @@
 from telegram import Bot
-from core.manage.settings import TELEGRAM, COMMANDS
+from core.manage import settings 
 import asyncio
 import time
 
@@ -9,12 +9,13 @@ class TelegramCustomBot:
 
     @staticmethod
     async def _set_url():
-        await cls.__instance.bot.set_webhook(url=TELEGRAM.get('WEBHOOK_URL'),max_connections=50,read_timeout=10)
+        print('RESETEO')
+        await TelegramCustomBot.__instance.bot.set_webhook(url=settings.TELEGRAM.get('WEBHOOK_URL'))
 
     def __new__(cls, *args, **kwargs):
         if not cls.__instance:
             cls.__instance = super().__new__(cls)
-            cls.__instance.bot = Bot(token=TELEGRAM.get('API_KEY'))
+            cls.__instance.bot = Bot(token=settings.TELEGRAM.get('API_KEY'))
             cls.__LOOP.create_task(cls._set_url()) # Thread
         return cls.__instance
 
@@ -31,7 +32,7 @@ class TelegramCustomBot:
 
     @command.setter
     def command(self,command):
-        self.__command = command if (command in COMMANDS.keys()) else 'default'
+        self.__command = command if (command in settings.COMMANDS.keys()) else 'default'
 
     @property
     def last_message_id(self):
@@ -80,6 +81,6 @@ class TelegramCustomBot:
 
     async def send_message(self):
         self.command = self.current_message.text
-        await self.__instance.bot.send_message(chat_id=TELEGRAM.get('CHAT_ID'), text=self._verify_command())
+        await self.__instance.bot.send_message(chat_id=settings.TELEGRAM.get('CHAT_ID'), text=self._verify_command())
 
 
