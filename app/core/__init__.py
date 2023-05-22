95c783056da55f9efb0f8b9d3ffb8ddb702f10d3
john62821 <johndavid62821@gmail.com>
2023-05-21 20:13:15 -0400

    ADD: Root redirect

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from starlette.responses import RedirectResponse
 from core.manage import settings
 from api.v1.controllers.telegram_bot_webhook import webhooks_router
 
@@ -12,6 +13,11 @@ app = FastAPI(
         openapi_tags=settings.TAGS_METADATA,
         docs_url="/api-docs",
     )
+
+# Redirect
+@app.get("/", include_in_schema=False)
+async def root():
+    return RedirectResponse(url="/api-docs")
 
 # Routers
 app.include_router(webhooks_router, prefix="/api/v1")
