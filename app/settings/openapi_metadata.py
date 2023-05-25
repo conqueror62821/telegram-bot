41bc6429de42a9083034653efbd5e5b902fbd5be
john62821 <johndavid62821@gmail.com>
2023-05-24 21:02:30 -0400

    ADD: Open api metadata

---
+++ app/settings/openapi_metadata.py
@@ -0,0 +1,12 @@
+# API version
+VERSION = {
+    1 : '/v1',
+    2 : '/v2',
+}   
+
+TAGS_METADATA = [
+    {
+        "name": "Webhooks v1",
+        "description": "Version 1.0 de Webhooks router."
+    }
+]
