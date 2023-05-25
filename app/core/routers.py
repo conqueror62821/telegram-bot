67ccb110aa95cadfa3ec5cefd9258cee17b1071f
john62821 <johndavid62821@gmail.com>
2023-05-24 21:03:37 -0400

    UPDATE: Routers app

--- app/core/routers.py
+++ app/core/routers.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,FastAPI
+from api.v1.controllers import telegram_bot_webhook
+from core.manage import settings
 
-webhooks_router = APIRouter()
-
+# Global routers
+def setup_routes(app : FastAPI):
+    global_router = APIRouter()
+    global_router.include_router(telegram_bot_webhook.router,prefix=settings.VERSION.get(1))
+    app.include_router(global_router, prefix="/api")
