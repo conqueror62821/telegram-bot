b9f1eddea9d3c3090e612cfb5b830a83071fa126
john62821 <johndavid62821@gmail.com>
2023-05-28 08:58:46 +0000

    ADD: Timezone and media

--- app/settings/base.py
+++ app/settings/base.py
@@ -22,4 +22,19 @@ TELEGRAM = {
 }
 
 
+GIPHY = {
+    'API_KEY' : config('GIPHY_API_KEY'),
+    'GIF_BASE_URL' : 'https://api.giphy.com/v1/gifs',
+    'STICKER_BASE_URL' : 'https://api.giphy.com/v1/stickers',
+}
 
+
+TIMEZONE = 'Chile/Continental'
+
+
+ALLOW_ORIGINS = ['http://0.0.0.0:8000']
+ALLOW_CREDENTIALS = True
+ALLOW_METHODS = ['*']
+ALLOW_HEADERS = ['*']
+EXPOSE_HEADERS = ''
+MAX_AGE = ''
