7836be9dc09c6bdbc5e7b1b914e9bff2443f8775
john62821 <johndavid62821@gmail.com>
2023-05-30 07:08:39 +0000

    ADD: New endpoint send message

--- app/api/v1/controllers/telegram_bot_webhook.py
+++ app/api/v1/controllers/telegram_bot_webhook.py
@@ -1,11 +1,34 @@
-from fastapi import Request,APIRouter,status
+from fastapi import Request,APIRouter,status,HTTPException,UploadFile,File,Form
 import telegram
 import websockets
 import json
 from core.logger import logger
 from fastapi.responses import Response
-from core.init_bot import bot
+from core.manage import settings 
+from pydantic import BaseModel, validator,root_validator
+from typing import Optional
+from telegram import Update
+from telegram.ext import *
 
+
+from models.telegram_handler import telegram_app
+
+# TODO: Refac endpoints api
+class BaseMessage(BaseModel):
+    chat_id: Optional[str] 
+
+    @property
+    def model_name(self) -> str: return 'message'
+    
+    @root_validator(pre=True)
+    def validate_fields(cls, values):
+        chat_id = values.get('chat_id')
+        if not chat_id: 
+            values['chat_id'] = settings.TELEGRAM.get('CHAT_ID')
+        return values
+
+class RequiredTextMessage(BaseMessage):
+    text: str
 
 router = APIRouter(
     prefix='/webhooks',
@@ -19,25 +42,29 @@ router = APIRouter(
     description="Telegram messages webhook.",
 )
 async def webhook(request: Request):
-    update = telegram.Update.de_json(await request.json(), bot)
-
-    message = update.message
-
-    # Skip messages
-    if message.message_id == bot.last_message_id: return
-    
-    # Process
-    bot.last_message_id = message.message_id
-    bot.current_message = message
-
-    data = {'response' : True,'chat_id': bot.last_message_id}
-    
-    logger.info(f'message received from {bot.current_message.from_user.first_name} {bot.current_message.from_user.last_name}.')
-
-    # send data to WebSocket
-    async with websockets.connect('ws://0.0.0.0:8000/ws') as websocket:
-        await websocket.send(json.dumps(data))
+    update = Update.de_json(await request.json(), telegram_app.bot)
+    response = await telegram_app.handle_message(update)
+    if not response: raise HTTPException(status_code=400, detail="The request could not be processed")
     return Response(status_code=204)
 
 
+@router.post(
+    '/telegram/bot/send_message',
+    response_model=None,
+    description='Send message from bot.',
+)
+async def send_telegram_message(message: RequiredTextMessage):
+    print(message.chat_id)
+    response = await telegram_app.bot.send_message(chat_id=message.chat_id,text=message.text)
+    if not response: raise HTTPException(status_code=400, detail="Message not sent")
+    content = json.dumps(
+        {
+            'response': True,
+            'message': f'Message sent sucessfully.',
+            'body': {
+                message.model_name : message.dict()
+            }
+        }
+    )
+    return Response(status_code=201,content=content, media_type="application/json")
 
