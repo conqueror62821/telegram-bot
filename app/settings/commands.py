04508b4de33b42e077cbef6a028bb6304cd82b3e
john62821 <johndavid62821@gmail.com>
2023-05-30 07:04:40 +0000

    ADD: Global commands

--- app/settings/commands.py
+++ app/settings/commands.py
@@ -1,18 +1,28 @@
-from random import randint as rand
+from models.custom_command import CustomCommand
+from telegram import BotCommand
 
-KAOMOJIS = ['(≧◡≦) ♡','♡(>ᴗ•)','૮ ˶ᵔ ᵕ ᵔ˶ ა']
+# TODO: Commads
 
-
-# TODO: ADD NEW STATIC AND DYNAMIC COMMANDS
 COMMANDS = {
-    '!help' : {
-        'description' : 'Listado de comandos.',
-        'template' : '',
-        'type' : 'dynamic'
+    '/help':{
+        'command': BotCommand(command='/help', description='Lista de comandos disponibles ♡'),
+        'function': CustomCommand.help_command(),
+        'type': 'reply_text'
     },
-    '!x4leqxinn' : {
-        'description' : 'Invoca una mona xina',
-        'template' : f'Mi creador {KAOMOJIS[2]}',
-        'type' : 'static',
-    }
+    '/x4leqxinn':{
+        'command': BotCommand(command='/x4leqxinn', description='♱𖤐⋆ Creador ૮ ˶ᵔ ᵕ ᵔ˶ ა'),
+        'function': CustomCommand.creator_command(),
+        'type': 'reply_text'
+    },
+    '/about':{
+        'command': BotCommand(command='/about', description='Acerca de este bot (｡♡‿♡｡)'),
+        'function': CustomCommand.about_command(),
+        'type': 'reply_text'
+    },
+    '/onichan':{
+        'command': BotCommand(command='/onichan', description='No lo hagas x tu salud mental ૮ ˶•~•˶ ა'),
+        'function': CustomCommand.onichan_command,
+        'type': 'custom'
+    },
 }
+
